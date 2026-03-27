3bc4b2292b82718da967c6159a3ac7def6dc248d
Markus Winter <m.winter@sap.com>
2026-03-27 10:37:40 +0100

    Use dialog to add/edit the description
    The current approach sends a request to the backend to fetch the form
    then evaluates the html to load any potential javascript, hides the
    current description and instead shows the loaded form.
    That is not the bext user experience especially with the new UI design.
    
    This change will use a dialog to edit the description.
    
    fixes #26488

--- core/src/main/resources/lib/hudson/editable-description.js
+++ core/src/main/resources/lib/hudson/editable-description.js
@@ -1,5 +1,16 @@
 /* global replaceDescription */
 (function () {
+  function editDescription(button) {
+    const template = document.getElementById("edit-description-template");
+    const title = button.dataset.title;
+    const form = template.content.firstElementChild.cloneNode(true);
+
+    dialog.form(form, {
+      title: title,
+      okText: dialog.translations.save,
+    });
+  }
+
   document.addEventListener("DOMContentLoaded", function () {
     const descriptionLink = document.querySelector("#description-link");
     const description = document.getElementById("description");
@@ -7,32 +18,8 @@
       descriptionLink.classList.remove("jenkins-hidden");
       descriptionLink.addEventListener("click", function (e) {
         e.preventDefault();
-        descriptionLink.classList.add("jenkins-hidden");
-        let url = descriptionLink.getAttribute("data-url");
-        let description = descriptionLink.getAttribute("data-description");
-        return replaceDescription(description, url);
+        editDescription(descriptionLink);
       });
     }
   });
-
-  Behaviour.specify(
-    ".description-cancel-button",
-    "description-cancel-button",
-    0,
-    function (b) {
-      b.onclick = function () {
-        const descriptionLink = document.getElementById("description-link");
-        const descriptionContent = document.getElementById(
-          "description-content",
-        );
-        const descriptionEditForm = document.getElementById(
-          "description-edit-form",
-        );
-        descriptionEditForm.innerHTML = "";
-        descriptionEditForm.classList.add("jenkins-hidden");
-        descriptionContent.classList.remove("jenkins-hidden");
-        descriptionLink.classList.remove("jenkins-hidden");
-      };
-    },
-  );
 })();
