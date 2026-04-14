7e195ade9cc9f70b6ed87722ddad2ffab69b6b37
Markus Winter <m.winter@sap.com>
2026-04-15 00:52:24 +0200

    use agent-subpage for disconnect, mark offline and update offline pages

--- core/src/main/java/jenkins/model/details/MonitoringDetails.java
+++ core/src/main/java/jenkins/model/details/MonitoringDetails.java
@@ -2,7 +2,7 @@ package jenkins.model.details;
 
 import hudson.model.Computer;
 
-public class MonitoringDetails extends Detail{
+public class MonitoringDetails extends Detail {
     public MonitoringDetails(Computer computer) {
         super(computer);
     }
